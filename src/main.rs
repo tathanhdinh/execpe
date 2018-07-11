6cacca00f091a4fd99ec9338ac529e12391fe96a
TA Thanh Dinh <tathanhdinh@gmail.com>
2018-07-11 18:36:49 +0200

    fix CreateProcess arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// cargo build --release --target i686-pc-windows-msvc
+
 #![windows_subsystem = "console"]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
@@ -6,10 +8,10 @@ extern crate winapi;
 #[macro_use] extern crate structopt;
 #[macro_use] extern crate failure;
 
-use std::{result, path, ffi::OsString, os::windows::ffi::OsStrExt, iter, ptr};
+use std::{result, path, ffi::OsString, os::windows::ffi::OsStrExt, iter, ptr, mem};
 use failure::Error;
 use structopt::StructOpt;
-use winapi::um::{processthreadsapi::CreateProcessW, errhandlingapi::GetLastError};
+use winapi::um::{processthreadsapi::{CreateProcessW, STARTUPINFOW, PROCESS_INFORMATION}, errhandlingapi::GetLastError, winnt};
 
 type Result<T> = result::Result<T, Error>;
 
@@ -25,25 +27,27 @@ struct ExecPeArg {
 fn main() -> Result<()> {
     let ret = unsafe {
         let arg = ExecPeArg::from_args();
-        let lpApplicationName = OsString::from(arg.pe_file).encode_wide()
-                                                           .chain(iter::once(0))
-                                                           .collect::<Vec<u16>>();
-        CreateProcessW(lpApplicationName.as_ptr(), 
-                       ptr::null_mut(),     // lpCommandLine
-                       ptr::null_mut(),     // lpProcessAttributes
-                       ptr::null_mut(),     // lpThreadAttributes
-                       0,                   // bInheritHandles
-                       0,                   // dwCreationFlags
-                       ptr::null_mut(),     // lpEnvironment
-                       ptr::null_mut(),     // lpCurrentDirectory
-                       ptr::null_mut(),     // lpStartupInfo
-                       ptr::null_mut())     // lpProcessInformation
+        let pe_name = OsString::from(arg.pe_file).encode_wide()
+                                                 .chain(iter::once(0))
+                                                 .collect::<Vec<u16>>();
+
+        let mut startupInfo: STARTUPINFOW = mem::zeroed();
+        let mut processInfo: PROCESS_INFORMATION = mem::zeroed();
+
+        CreateProcessW(ptr::null_mut(),                   // lpApplicationName
+                       pe_name.as_ptr() as winnt::LPWSTR, // lpCommandLine
+                       ptr::null_mut(),                   // lpProcessAttributes
+                       ptr::null_mut(),                   // lpThreadAttributes
+                       0,                                 // bInheritHandles
+                       0,                                 // dwCreationFlags
+                       ptr::null_mut(),                   // lpEnvironment
+                       ptr::null_mut(),                   // lpCurrentDirectory
+                       &mut startupInfo,                  // lpStartupInfo
+                       &mut processInfo)                  // lpProcessInformation
     };
 
     if ret == 0 {
-        let last_err_code = unsafe {
-            GetLastError()
-        };
+        let last_err_code = unsafe { GetLastError() };
         return Err(format_err!("cannot execute PE file (error code: {})", last_err_code))
     }
 
